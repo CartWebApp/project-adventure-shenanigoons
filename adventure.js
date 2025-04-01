68a080e301f57100ee64980566dbe9cf68a168bf
Schroeder Ryan <142847750+ThatWierdAnimator@users.noreply.github.com>
2025-04-01 10:45:57 -0700

    a

--- adventure.js
+++ adventure.js
@@ -3,26 +3,47 @@ const options = document.getElementById('options');
 const input = document.getElementById('input');
 const submit = document.getElementById('submit');
 
-const test = {
-    text: 'You are taking out the trash at your job from good burger when hatsune miku appears in a doctor strange type portal and kicks master oogway out of her universe. She is real intimidating and master oogway tells you to go after her before she escapes',
-    options: ['Paralyzed with fear', 'Go after her', '"What is going on?!"'],
-    scenes: [
-        {
-            text: 'Miku laughs and says smth like "this?! this is your hero? you suck lmao"',
-            options: ['Continue'],
-            scenes: [[]]
-        }
-    ],
-    init: function () {
-        for (i in this.scenes) {
-            this.scenes[i].parent = this;
-        }
-        delete this.init;
-        return this;
-    }
-}.init();
+function StoryObject(text, options, scenes, path) {
+    this.text = text;
+    this.options = options;
+    this.scenes = scenes;
+    this.path = path;
+}
 
-Object.prototype.run = function () {
+const test = new StoryObject(
+    'You are taking out the trash at your job from good burger when hatsune miku appears in a doctor strange type portal and kicks master oogway out of her universe. She is real intimidating and master oogway tells you to go after her before she escapes',
+    ['Paralyzed with fear', 'Go after her', '"What is going on?!"'],
+    [
+        new StoryObject(
+            'Miku laughs and says smth like "this?! this is your hero? you suck lmao"',
+            ['Continue'],
+            [new StoryObject(0, 0, 0, ['p'])]
+        )
+    ]
+);
+
+
+
+// const test = {
+//     text: 'You are taking out the trash at your job from good burger when hatsune miku appears in a doctor strange type portal and kicks master oogway out of her universe. She is real intimidating and master oogway tells you to go after her before she escapes',
+//     options: ['Paralyzed with fear', 'Go after her', '"What is going on?!"'],
+//     scenes: [
+//         {
+//             text: 'Miku laughs and says smth like "this?! this is your hero? you suck lmao"',
+//             options: ['Continue'],
+//             scenes: [[]]
+//         }
+//     ],
+//     init: function () {
+//         for (i in this.scenes) {
+//             this.scenes[i].parent = this;
+//         }
+//         delete this.init;
+//         return this;
+//     }
+// }.init();
+
+StoryObject.prototype.run = function () {
     text.innerHTML = this.text;
     options.innerHTML = '';
     for (option of this.options) {
@@ -30,12 +51,36 @@ Object.prototype.run = function () {
     }
 }
 
-if (typeof thing === 'array') {
-    let current = test;
-    for (index of thing) {
-        current = current.scenes[index];
+StoryObject.prototype.addParents = function () {
+    for (scene of this.scenes) {
+        if (typeof scene === 'string') {
+            console.log('grumbus');
+        } else {
+            scene.parent = this;
+        }
     }
-    return current;
 }
 
-test.scenes[0].scenes[0].run();
+// get a mapping function for the path
+StoryObject.prototype.findPath = function () {
+    function followPath(object, path) {
+        if (path[0] === 'p') {
+            path.shift();
+            followPath(object.parent, path);
+        } else if (path.length === 0) {
+            return object;
+        } else {
+            path.shift();
+            followPath(object.scenes[path[0]], path);
+        }
+    }
+
+    console.log(followPath(this.parent, this.path));
+    console.log(this.path);
+    console.log(this);
+}
+
+test.addParents();
+test.scenes[0].addParents();
+test.scenes[0].scenes[0].findPath();
+// test.scenes[0].parent.run();
