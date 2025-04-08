a49e0b579eca64ab00e3454f4e9d055ea9343a71
Schroeder Ryan <142847750+ThatWierdAnimator@users.noreply.github.com>
2025-04-08 08:59:27 -0700

    absolute sekai

--- adventure.js
+++ adventure.js
@@ -2,18 +2,37 @@ const text = document.getElementById(`text`);
 const options = document.getElementById(`options`);
 const input = document.getElementById(`input`);
 const submit = document.getElementById(`submit`);
+const image = document.getElementById(`image`);
 
-// function StoryObject(text, options, scenes, path, item, secret) {
-//     this.text = text;
-//     this.options = options;
-//     this.scenes = scenes;
-//     this.path = path;
-//     this.item = item;
-//     this.secret = secret;
-// }
+// I'm templating it I'm templating it I'm templating it I'm templating it I'm templating it I'm templating it 
 
-let inventory = {
-    supersize: true
+/* 
+    {
+        text: `story text to be displayed`,
+        options: [`choice 1`, `choice 2`],
+        scenes: [
+            {
+                this is the scene selected by choice 1
+            },
+            {
+                this is the scene selected by choice 2
+            }
+        ],
+        image: `the image file to be displayed`,
+        // secret appends an option to the options array if the item in the inventory is true
+        // so supersize shows as an option if supersize is true
+        secret: {item: supersize, option: `Supersize`},
+        item: `string of the item to be added to the inventory`,
+        ending: true or false
+        
+        // if want scene to go up to parent and down path { path: ['p', (num of option), ...] }
+
+        // mabye add fonts perchance
+    }
+*/
+
+const inventory = {
+    
 };
 
 const story = {
@@ -28,6 +47,7 @@ const story = {
     <br><br> Miku stepped forward, crossing into your world.
     You had no choice.`,
     options: [`Stay paralyzed, stunned with fear `, `Go after her `, `You exclaim, “What is going on!?” `],
+    image: `absolute-sekai.webp`,
     scenes: [
         {
             text: `You couldn’t move. You couldn’t speak.
@@ -46,7 +66,7 @@ const story = {
             The air pulsed. A sharp force hurled you into split garbage bags.
             Miku loomed over you. “Should I bother finishing this… or have you learned your place?”`,
             options: [`Continue`],
-            scenes: [{path: ['p', 3] }]
+            scenes: [{ path: ['p', 3] }]
         },
         {
             text: `Dragging in a shaky breath, you took a step back, eyes darting between the still-crackling portal and the dumpster where Master Oogway had just crash-landed. Your heart pounded, your brain scrambling to process what you'd just witnessed.
@@ -56,7 +76,7 @@ const story = {
             You swallowed hard, taking a shaky step back. The air around her shimmered, distorting like heat waves, her very existence too overwhelming for reality to contain.
             She took a slow step forward. “But I will say this—you're in the wrong place, at the worst possible time.” Her fingers twitched, and a surge of energy crackled in the air between you.`,
             options: [`Continue`],
-            scenes: [{path: ['p', 3] }]
+            scenes: [{ path: ['p', 3] }]
         },
         {
             text: `Miku scanned her surroundings, taking in the neon signs, the flickering streetlights, the distant hum of traffic. A slow, satisfied smile crossed her face.
@@ -81,8 +101,81 @@ const story = {
                         secret: { item: `supersize`, option: `Supersize` }
                     }
                 ]
+            },
+            {
+                text: `Oogway opens a portal to a peaceful world.
+                “Now, it’s time you learnt skadoosh” Oogway says
+                He takes 40 days and 40 nights to teach you, but it’s okay since time does not pass in this dimension
+                You arrive back in your world after your training, and are ready to take Miku down<br><br>
+                You now know skadoosh!`,
+                options: [`Continue`],
+                scenes: [{
+                    text: `You are in front of the miku home base, the miku-scraper. It appears like she's already started to take over your world. Your companion explains that miku has set up a field around the skyscraper to block any portals from opening inside`,
+                    options: [`Continue`],
+                    scenes: [{
+                        text: `Oogway advises that you both enter from the basement, to avoid any real conflict. You agree with your mentor, and he creates a portal inside the sewers just outside the base's basement. You ask, "Wait, how do we get in?" Oogway explains that the entrance is a secret. An open secret. Anyone can find it. Oogway opens the wall like a doorway, and goes inside. You follow him. Inside this basement you see:`,
+                        options: [`A box of goodies`, `A mall type of map (you are here type thing)`, `An elevator`, `A staircase`],
+                        scenes: [{
+                            text: `You gain an odd looking key. There is a container of a viscous purple fluid at the top of the key<br><br>OBTAINED ELIXER KEY`,
+                            options: [`Continue`],
+                            scenes: [{ path: ['p'] }],
+                            item: `elixer-key`
+                        }, {
+                            text: `The map shows "you are here" in the basement. The map shows that the skyscraper is terribly unorganized, with a web diagram tangled over the whole thing showing many paths for portals. Oogway says this is an attempt at confusing any intruders, but he's seen many miku-scrapers in his day, so he'll guide you through this one easily. He points to the map and says it's best to take the elevator here`,
+                            options: [`Continue`],
+                            scenes: [{ path: ['p'] }]
+                        }, {
+                            text: `You press the button and wait for the elevator to open. As it does, you see a few of miku's soldiers inside. They both recognize Oogway and turn to him. They immediatly ask for an autograph. Oogway chuckles and grants their wish. "You know, peace is always the best option to take, if the oppurtunity presents itself." You heed the words of your master and continue. The elevator leads straight up to the second highest floor before stopping. "This is as high as it will go. I mean it'll go higher if we had more authority, but this is pretty good all things considered." You see a reception desk guarding an intimidating staircase. The person manning the desk is LeBron. "Do y'all have an appointment?"`,
+                            options: [`Yes, of course!`, `No. We are here to stop Miku`],
+                            scenes: [{
+                                text: `LeBron eyes you disapprovingly. "Guys, I'm the goat. You really think I can't see through a simple little lie like that?" Lebron grabs you Omni-man style. "And to think I thought you were worthy enough to stop Miku. I want to make this world a better place. And that starts by removing you from it" As LeBron says this, he flies and crashes through the window. He holds you over the street, about 2,000 ft in the air. He drops you (if you've been dropped in another ending he says "heh. feels like we've done this before, huh?").<br><br>YOU DIE`,
+                                ending: true
+                            }, {
+                                text: `LeBron chuckles a little bit. "Yeah, I was getting tired of her reign too. Y'all are free to head on back there, do whatever you want lmao" You both head up the staircase and see Miku, sitting with her back to you both as she looks at the skyline. She doesn't say anything. Oogway: "She knows we're here. There's no point in hiding this." Miku is startled, and looks back at the both of you. "Since when did you two get in here?? None of my alarms went off! No matter. There's no way either of you could stop me now. I'm so deeply rooted in everyone's wifi that there's no point in even attempting to halt my expansion."`,
+                                options: [`Ask Oogway for help`, `Nuh uh!! (Attack)`],
+                                scenes: [{
+                                    text: `Oogway knows you can take her in a fight, but decides against it. You are reminded of Oogway's words, and also decide that peace would be a better option. How do you attempt to convince her not to conquer this world?`,
+                                    options: [`Do a flip`, `Offer a game of slots`, `Talk Miku out of it`],
+                                    scenes: [{
+                                        text: `Miku lets out an obvious pog, but her decision is unwavering. She decides to take your life.<br><br>YOU DIE`,
+                                        ending: true
+                                    }, {
+                                        text: `You explain the concept of slots, and Miku is intrigued. The thought of making money easily has piqued her interest. She demands you show her the way, and offers a vehicle. It's one of the cars from fortnite. You both make it to Vegas, and Miku is in awe of everything. She decides to spare this world, because of this beautiful game. You end up gambling a bunch with Miku, and both end up as best friends by the end of it.<br><br>YOU REACHED THE GAMBLING ENDING`,
+                                        ending: true
+                                    }, {
+                                        text: `You begin to imitate Steven Universe. "Miku... this isn't like you!" Miku, to be frank, is insulted that you would ever say such a thing and Miku Miku beams you on the spot.<br><br>YOU DIE`,
+                                        ending: true
+                                    }]
+                                },{
+                                    // rhythm game
+                                }]
+                            }]
+                        }, {
+                            text: `You walk up the staircase with Master Oogway to see a group of high level intimidating guards on the ground floor`,
+                            options: [`Continue`],
+                            secret: {item: `drip`, option: `Whip out the drip`},
+                            scenes: [{
+                                text: `Without warning, they attack, giving no time for you to think. This must have been why Oogway said to avoid this floor. You have learned your lesson.<br><br>YOU DIE`,
+                                ending: true
+                            }, {
+                                text: `You pull out the Miku drip and they are all taken aback. Peter Griffin looks you up and down, and says, "Heheh this is like that time we did the drip contest." Suddenly, your vision cuts out and you see Miku standing in front of a group of people. As you look closer, you see that these people are actually those same guards, but they are all dripped out the wazoo. They seem to be having a contest for who's the drippiest. Miku, of course, wins this with her savage lil' drip coat. Your vision comes back, and all the guards are laughing remembering their good times. Peter walks over to you and puts a hand on your shoulder. "Hey kid, why don't you work with us? Times like this don't happen often, but when they do, it's the best time ever. I can't believe I ever forgot about this."`,
+                                options: [`Yeah I'll take the job`, `Nah I'm good. Thanks tho!`],
+                                scenes: [{
+                                    text: `"Good choice, kid!" As one of Miku's guards, you go on to rank pretty high in the group. After a while, you get to the point where you get to host monthly drip contests. Miku judges all of these, of course, but you still have the honor of giving everyone the oppurtunity to express themselves and unwind. You can't believe you ever thought of taking these people down, and removing their culture from this planet. Also Oogway is executed for being a traitor<br><br>YOU REACHED THE DRIP ENDING<br>OPEN SECRET`,
+                                    ending: true
+                                }, {
+                                    text: `Peter says, "No worries kid.  But if you're trying to get to Miku, go down and take the elevator."`,
+                                    options: [`Take the elevator`],
+                                    scenes: [{path: ['p', 'p', 'p', 2]}]
+                                }]
+                            }]
+                        }]
+                    }]
+                }]
             }]
-        }
+        },
+        // the debug pathway
+        { path: [3, 1, 0, 0] }
     ]
 };
 
@@ -96,6 +189,9 @@ Object.prototype.run = function () {
         for (option of this.options) {
             options.innerHTML += `<li>${option}</li>`
         }
+    }
+    if ('image' in this) {
+        image.src = this.image;
     }
 
     if (this.item) {
